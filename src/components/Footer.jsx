595caeb1b39d75fea8b1b9f233ac6ccae4fc870b
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-10-09 01:10:08 +0600

    added footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,67 @@
-import React from 'react';
+import React from "react";
+import Logo from "./../assets/logo.png";
+import "./../index.css";
+import { NavLink } from "react-router";
+import { BsLinkedin } from "react-icons/bs";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { IoLogoYoutube } from "react-icons/io5";
 
 const Footer = () => {
     return (
-        <h1>Header</h1>
+        <footer className="bg-black text-white">
+            <div className="footer sm:footer-horizontal p-10 w-11/12 mx-auto pl-0 pr-0">
+                <aside>
+                    <NavLink src="/" className="flex items-center no-underline">
+                        <img
+                            src={Logo}
+                            alt="App Logo"
+                            style={{ width: "70px", height: "70px" }}
+                        />{" "}
+                        <span className="text-purple-500 font-bold text-3xl pl-3">
+                            HERO.IO
+                        </span>{" "}
+                    </NavLink>
+                    <p className="pr-14 text-gray-400">
+                        Lorem Ipsum is simply dummy text of <br />
+                        the printing and typesetting industry. <br />
+                        Lorem Ipsum has been the industry's <br />
+                        standard dummy text ever since the <br /> 1500s,when an unknown
+                        printer took <br /> a galley of type and scrambled it to <br /> make
+                        a type specimen book.
+                    </p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Social</h6>
+                    <div className="grid grid-flow-col gap-4">
+                        <a href="#" className="text-2xl">
+                            <BsLinkedin />
+                        </a>
+                        <a href="#" className="text-2xl">
+                            <FaFacebook />
+                        </a>
+                        <a href="#" className="text-2xl">
+                            <FaSquareXTwitter />
+                        </a>
+                        <a href="#" className="text-2xl">
+                            <IoLogoYoutube />
+                        </a>
+                    </div>
+                </nav>
+            </div>
+            <div className="footer sm:footer-horizontal footer-center p-7  text-white">
+                <p className=" text-gray-400">
+                    Copyright © {new Date().getFullYear()} - HERO.IO. All rights reserved.
+                </p>
+            </div>
+        </footer>
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className="navbar bg-base-100  w-11/12 mx-auto pl-0 pr-0">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden pl-0">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
