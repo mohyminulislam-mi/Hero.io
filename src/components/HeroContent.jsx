6e0e7fc7744b4169611269379d5db6c40ddb8627
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-10-10 22:56:42 +0600

    make some changes

--- src/components/HeroContent.jsx
+++ src/components/HeroContent.jsx
@@ -9,8 +9,8 @@ const HeroContent = () => {
         <div>
              {/* ----- hero contents start here----  */}
             <div className='text-center mt-20'>
-                <h1 className='text-5xl font-bold lg:px-[450px] lg:py-[15px]'>We Build <span className='text-6xl text-purple-500'>Productive</span> Apps</h1>
-                <p className='lg:px-[310px]  p-[15px] text-[#627382]'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+                <h1 className='text-5xl font-bold lg:py-[15px]'>We Build <span className='text-6xl text-purple-500'>Productive</span> Apps</h1>
+                <p className=' p-[15px] text-[#627382]'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
                 <div className='flex items-center justify-center mt-8'>
                     <Link to={'https://play.google.com/store/apps'} target="_blank" className='border-gray-300 border flex justify-center items-center mr-7 py-[5px] px-4'>
                         <img src={Playstore} />
