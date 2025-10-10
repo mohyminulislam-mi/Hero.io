9d908b64774e1964afc4a06ffc223c6044574864
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-10-10 18:22:07 +0600

    added Error and App-Not-Found

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import useData from '../Hooks/useData';
 import App from '../components/App';
+import AppNotFound from '../components/AppNotFound';
 
 
 const Apps = () => {
@@ -17,14 +18,15 @@ const Apps = () => {
                 <p className='text-[#627382]'>Explore All Apps on the Market developed by us. We code for Millions</p>
             </div>
             <div className='w-11/12 mx-auto flex justify-between items-center pb-5'>
-                <h1>All Apps {' '} <span>({searcheApps.length}) Apps Founds</span></h1>
+                <h1 className='font-semibold'>{' '} <span>({searcheApps.length}) Apps Founds</span></h1>
                 <label className='input'>
                     <input value={search} onChange={e => setSearch(e.target.value)} type="search" placeholder='Search Apps' />
                 </label>
 
             </div>
             <div className='w-11/12 mx-auto grid grid-cols-2  lg:grid-cols-4 gap-4'>
-                {searcheApps.map(app => <App key={app.id} app={app}></App>)}
+                {
+                  searcheApps.length ? searcheApps.map(app => <App key={app.id} app={app}></App>) : <AppNotFound></AppNotFound>}
             </div>
         </div>
 
--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { useRouteError } from 'react-router';
+import { Link, useRouteError } from 'react-router';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import error404 from '../assets/error-404.png'
 
 const Error = () => {
     const error = useRouteError();
-    
+
     return (
         <div className='flex flex-col min-h-screen'>
             <Navbar></Navbar>
             <div className='flex-1 w-11/12 mx-auto'>
-                <h1>404 Error! {error.message}</h1>
+                <div className='p-15 text-center'>
+                    <div className='flex justify-center mb-5'><img className='text-center' src={error404} alt={error.message} /></div>
+                    <h1 className='font-bold text-4xl'>Oops, Page not found!</h1>
+                    <p className='text-[#627382] my-2'>The page you are looking for is not available.</p>
+                    <Link to="/"
+                        className="btn bgbtn h-10 bg-gradient-to-r from-indigo-500 to-purple-500 text-white mt-5"
+                    > Back to Home</Link>
+                </div>
             </div>
             <Footer></Footer>
         </div>
